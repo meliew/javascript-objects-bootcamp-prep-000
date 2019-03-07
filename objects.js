ecc6b8cc38f5defc2089772bde0fb6a3b6fd3d1e
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-07 00:39:03 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist = { madonna: "like a prayer"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
 
-  return playlist.assign({}, playlist, { [artistName]: songTitle })
+  return Object.assign({}, playlist, { [artistName]: songTitle })
 
 }
