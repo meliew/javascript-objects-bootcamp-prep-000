794290663e90f144bf43c4b00c1452f14e59a171
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-07 01:02:24 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,7 @@ var playlist = { adele: "hello"};
 function updatePlaylist(playlist, artistName, songTitle) {
   // return Object.assign({}, playlist, { [artistName]: songTitle })
   playlist[artistName] = songTitle;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
